a0e7143608692386814babf4fd681790d8ba2b0c
PICOPICAPINOS <nachoharo44@gmail.com>
2016-10-16 00:56:21 +0200

    Ejercicio de CLASE

---
+++ base.js
@@ -0,0 +1,16 @@
+function suma(a,b){
+    if(arguments.length<2){
+        console.log("Hacen falta dos argumentos como mínimo.")
+    } else {
+        var result =0;
+        for (var i=0; i<arguments.length){
+            result += arguments[i];
+        }
+        return result;
+    console.log(result);
+    }
+}
+
+
+
+suma(18,2,4);
