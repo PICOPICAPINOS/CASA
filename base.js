34c44be4dee734c04b778a1d214c41adca5b9435
PICOPICAPINOS <nachoharo44@gmail.com>
2016-10-19 08:10:23 +0200

    La calculadora acabada sin funcionalidad

--- base.js
+++ base.js
@@ -1,16 +1,13 @@
-function suma(a,b){
-    if(arguments.length<2){
+function suma(a, b) {
+    var result = 0;
+    if (arguments.length < 2) {
         console.log("Hacen falta dos argumentos como mínimo.")
     } else {
-        var result =0;
-        for (var i=0; i<arguments.length;i++){
+        for (var i = 0; i < arguments.length; i++) {
             result += arguments[i];
         }
-        return result;
-    console.log(result);
+        console.log(result);
     }
 }
 // TODO: Anadirle la parte que verifica que el valor de entrada no es un numero.
-suma(2,2,2,2,2);
-suma(2,2);
-suma(18,2,4);
+suma(2,58);
